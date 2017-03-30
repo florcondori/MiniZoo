cf3f00c9de1975e910470d23c5f1be9302d5c311
flor condori <condorihuamanif@gmail.com>
2017-03-30 17:44:34 -0500

    se quita correctamente lA

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,31 @@ window.addEventListener('load', function(){
 	var selector = document.getElementById("selectores");
 
 	selector.addEventListener('change',function(){
-		for(var i=0; i<arrayImages.length; i++){
-			arrayImages[i].classList.add(selector.value);
-			
+		for(var i=0; i<arrayImages.length; i++){			
 			if(selector.value=="original"){
 				arrayImages[i].classList.remove("sepia");
-				arrayImages[i].classList.remove("brightness");
-				arrayImages[i].classList.remove("contrast");
 				arrayImages[i].classList.remove("grayscale");
 				arrayImages[i].classList.remove("invert");
 			}
+
+			if(selector.value=="grayscale"){
+				arrayImages[i].classList.add("grayscale");
+				arrayImages[i].classList.remove("invert");
+				arrayImages[i].classList.remove("sepia");			
+			}
+
+			if(selector.value=="sepia"){
+				arrayImages[i].classList.add("sepia");
+				arrayImages[i].classList.remove("grayscale");
+				arrayImages[i].classList.remove("invert");				
+			}
+
+			if(selector.value=="invert"){
+				arrayImages[i].classList.add("invert");
+				arrayImages[i].classList.remove("grayscale");
+				arrayImages[i].classList.remove("sepia");				
+			}
+
 		}
 
 	});
